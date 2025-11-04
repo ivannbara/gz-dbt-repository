1883bbb18b7c34358854848dcc05947ba8ac206e
ivann.baranowski@gmail.com <ivann.baranowski@gmail.com>
2025-11-04 13:30:39 +0000

    moufv

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,4 +1,4 @@
-select orders_id,date_date,round((margin+shipping_fee - (logcost+ship_cost)),2) as operational_margin from {{ ref("int_orders_margin") }}
+select orders_id,date_date,shipping_fee,logcost,ship_cost,round((margin+shipping_fee - (logcost+ship_cost)),2) as operational_margin from {{ ref("int_orders_margin") }}
 Left join {{ ref("stg_raw__ship") }} 
 using(orders_id)
 order by orders_id desc
