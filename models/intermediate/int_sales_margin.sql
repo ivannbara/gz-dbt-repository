de1b204ee2664061442a6fd965a5716add3c4bf7
Ivann Baranowski <ivann.baranowski@gmail.com>
2025-11-05 13:31:24 +0000

    eded

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,3 +1,7 @@
-select *, purchase_price*quantity as purchase_cost, round(revenue -(purchase_price*quantity),2) as margin from {{ ref("stg_raw__sales") }}
+select * 
+, purchase_price*quantity as purchase_cost
+, round(revenue -(purchase_price*quantity),2) as margin
+,{{margin_percent('revenue', 'purchase_price*quantity')}} as margin_percent
+ from {{ ref("stg_raw__sales") }}
 Left join {{ ref("stg_raw__product") }} 
 on products_id=pdt_id
