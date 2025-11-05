b6da862a416daaefc2e5f4921f567e201c797e68
Ivann Baranowski <ivann.baranowski@gmail.com>
2025-11-05 13:47:30 +0000

    big

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,7 +1,8 @@
-SELECT * FROM {{ ref("stg_raw__adwords") }}
-Union ALL
-SELECT * FROM {{ ref("stg_raw__bing") }}
-Union ALL
-SELECT * FROM {{ ref("stg_raw__criteo") }}
-Union ALL
-SELECT * FROM {{ ref("stg_raw__facebook") }}
+{{ dbt_utils.union_relations(
+    relations=[
+        ref('stg_raw__adwords'),
+        ref('stg_raw__bing'),
+        ref('stg_raw__criteo'),
+        ref('stg_raw__facebook')
+    ],
+) }}
